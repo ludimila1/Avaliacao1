87bd12d8baab06b7e052c6e2f923c1668fb91d64
ludimila1 <50583459+ludimila1@users.noreply.github.com>
2019-06-09 13:58:41 -0300

    Delete questao_20.py

--- questao_20.py
+++
@@ -1,5 +0,0 @@
-a=int(input("Digite o primeiro número: "))
-b=int(input("Digite o segundo número: "))
-c=int(input("Digite o terceiro número: "))
-soma=a+b+c
-print("A soma dos três termos é: ",soma)
