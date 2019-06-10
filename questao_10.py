d3264df9797e6d78d1d78d4a6f785636165b04ed
ludimila1 <50583459+ludimila1@users.noreply.github.com>
2019-06-09 21:21:25 -0300

    Delete questao_10.py

--- questao_10.py
+++
@@ -1,7 +0,0 @@
-user = input("Nome de usuario: ")
-senha = input("Senha: ")
-
-while senha == user:
-	senha = input("Digite uma senha diferente do nome de usuário: ")
-
-print ("Usuário: %s, Senha: %s" %(user, senha))
