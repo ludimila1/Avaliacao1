b83e7a6641ea104e586a7ed522b65f5693a9bd48
ludimila1 <50583459+ludimila1@users.noreply.github.com>
2019-06-09 21:25:18 -0300

    Delete questao_20.py

--- questao_20.py
+++
@@ -1,4 +0,0 @@
-a=str(input("Digite o primeiro argumento: "))
-b=str(input("Digite o segundo argumento: "))
-c=str(input("Digite o terceiro argumento: "))
-print("A soma dos três argumentos é: ",a+b+c)
